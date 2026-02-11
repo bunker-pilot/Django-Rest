f8a70578dfe202376de7518b789c2f76016532ec
Erfan Dousti <erfan.flashpoint@gmail.com>
2026-02-11 23:17:11 +0330

    Initial custom user

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,17 @@
-from django.db import models #noqa
+from django.db import models # noqa
+from django.contrib.auth.models import BaseUserManager, PermissionsMixin, AbstractBaseUser
 
-# Create your models here.
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        user = self.model(email=email ,**extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+class User(AbstractBaseUser, PermissionsMixin):
+    objects = UserManager()
+
+    email = models.EmailField(unique=True, max_length=250)
+    name = models.CharField(max_length=255)
+    is_active=  models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    USERNAME_FIELD = "email"
---
+++ app/core/tests/test_models.py
@@ -0,0 +1,13 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+
+    def test_create_user_success(self):
+        email = "test@example.com"
+        password = "Test2121!"
+        user = get_user_model().objects.create_user(email= email , password=password)
+
+        self.assertEqual(user.email , email)
+        self.assertTrue(user.check_password(password))
