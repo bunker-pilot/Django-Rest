47c4f8fe04259a72661b3f8a08531044c581b46e
Erfan Dousti <erfan.flashpoint@gmail.com>
2026-02-11 11:31:26 +0330

    Added wait_for_db command to make sure the database is ready before django runs

--- app/app/settings.py
+++ app/app/settings.py
@@ -11,10 +11,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -37,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "core",
 ]
 
 MIDDLEWARE = [
@@ -74,8 +74,11 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        "HOST": os.environ["DB_HOST"],
+        'NAME': os.environ["DB_NAME"],
+        "USER": os.environ["DB_USER"],
+        "PASSWORD": os.environ["DB_PASS"]
     }
 }
 
---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,25 @@
+"""
+Django command to wait for db to be available
+"""
+
+from django.core.management.base import BaseCommand
+import time
+from psycopg import OperationalError as PsycopgError
+from django.db.utils import OperationalError
+
+
+class Command(BaseCommand):
+    """Django command to wait for db"""
+
+    def handle(self, *args, **options):
+        """Entrypoint for command"""
+        self.stdout.write("Waiting for database...")
+        db_up = False
+        while db_up is False:
+            try:
+                self.check(databases=["default"])
+                db_up = True
+            except (OperationalError, PsycopgError):
+                self.stdout.write("Database is not available, waiting 1 second")
+                time.sleep(1)
+        self.stdout.write(self.style.SUCCESS("Database available!"))
