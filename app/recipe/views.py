ca9fb368415a2666dc80a8aef9082ed6c2e4672f
Erfan Dousti <erfan.flashpoint@gmail.com>
2026-02-22 16:07:20 +0330

    Added create recipe stuff

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,6 +1,7 @@
 """
 Views for the recipe api
 """
+
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
@@ -23,3 +24,6 @@ class RecipeViewSet(viewsets.ModelViewSet):
         if self.action == "list":
             return RecipeSerializer
         return self.serializer_class
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
